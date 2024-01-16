5045ddd220dbcab49a9d2f7f26c07fabbb4bda7b
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-16 15:24:35 -0300

    create class UserService

---
+++ picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuário do tipo Logista não está autorizado a realizar transação.");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente");
+        }
+
+    }
+
+    public User findUserById(Long id) throws Exception{
+
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+}
