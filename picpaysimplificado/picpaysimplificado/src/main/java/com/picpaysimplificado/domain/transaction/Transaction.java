f8b000e0c8f39035bd2f567f9c47ea2fc219199f
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-16 14:29:32 -0300

    class Transaction

---
+++ picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,33 @@
+package com.picpaysimplificado.domain.transaction;
+
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne // One User has many Transactions
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
