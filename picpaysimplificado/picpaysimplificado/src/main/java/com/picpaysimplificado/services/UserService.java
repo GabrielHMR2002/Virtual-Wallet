a459a1438f1bd472c0270e172b25e8070968b603
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-17 01:01:19 -0300

    createUse and getAllUsers

--- picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
+++ picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,11 +2,14 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -32,5 +35,15 @@ public class UserService {
 
     public void saveUser(User user){
         this.userRepository.save(user);
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+       return this.userRepository.findAll();
     }
 }
