b301aea1b4126095027fbb9409024a1be26b76bb
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-17 01:02:14 -0300

    Fixing the Transaction Class

--- picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -3,10 +3,7 @@ package com.picpaysimplificado.domain.transaction;
 
 import com.picpaysimplificado.domain.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity(name = "transaction")
 @Table(name = "transaction")
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
 
