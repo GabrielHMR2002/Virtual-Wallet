0db3e821f0ed57546fcd91c6ca41f06e9f64cd8c
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-17 01:03:04 -0300

    sendNotification method

--- picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/services/NotificationService.java
+++ picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -15,15 +15,17 @@ public class NotificationService {
 
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
-
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
-        ResponseEntity<String> notificationResponse = restTemplate.postForEntity
-                ("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
 
-        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
-            System.out.println("Erro ao enviar notificação");
-            throw new Exception("Serviço de notificação está fora do ar");
+//        ResponseEntity<String> notificationResponse = restTemplate.postForEntity
+//                ("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
+//
+//        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+//            System.out.println("Erro ao enviar notificação");
+//            throw new Exception("Serviço de notificação está fora do ar");
+//
+//        }
 
-        }
+        System.out.println("Notificação enviada para o usuário");
     }
 }
